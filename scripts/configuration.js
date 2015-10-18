d963e6c3bf5563c128fd66d69cd80d5a73db08bc
Craig McNamara <cmcnamara87@gmail.com>
2015-10-18 23:01:41 +1000

    Updates

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8888/api/v1/'})
+.constant('ENV', {name:'production',apiEndpoint:'http://bestintown.co/api/v1/'})
 
 ;
