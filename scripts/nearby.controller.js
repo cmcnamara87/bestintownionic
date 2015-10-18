9f4fddc5a9ecf011046b3268675217dd843a60b6
Craig McNamara <cmcnamara87@gmail.com>
2015-10-18 21:57:45 +1000

    Updates

---
+++ scripts/nearby.controller.js
@@ -0,0 +1,60 @@
+(function () {
+
+    'use strict';
+    angular
+        .module('bestintown')
+        .controller('NearbyController', NearbyController);
+
+    /* @ngInject */
+    function NearbyController($cordovaGeolocation,
+                              $ionicPlatform,
+                              $http,
+                              ENV) {
+        /* jshint validthis: true */
+        var vm = this;
+
+        vm.activate = activate;
+        vm.title = 'Nearby';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            $ionicPlatform.ready(function () {
+                getNearby();
+            });
+        }
+
+        function getNearby() {
+            var posOptions = {timeout: 10000, enableHighAccuracy: false};
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+                    var lat = position.coords.latitude
+                    var long = position.coords.longitude
+
+                    $http.get(ENV.apiEndpoint + 'nearby', {
+                        params: {
+                            lat: lat,
+                            lon: long
+                        }
+                    }).then(function (response) {
+                        vm.places = response.data;
+                    });
+
+                    //$http.get(ENV.apiEndpoint + 'hotspots', {
+                    //    params: {
+                    //        lat: lat,
+                    //        lon: long
+                    //    }
+                    //}).then(function (response) {
+                    //    vm.hotspots = response.data;
+                    //});
+                }, function (err) {
+                    // error
+                });
+        }
+    }
+
+})();
