ccdbdd2a56b4f56b0b288658b0aa993602f92b9e
Craig McNamara <cmcnamara87@gmail.com>
2015-10-31 11:59:52 +1000

    Updates

--- scripts/templates.js
+++ scripts/templates.js
@@ -78,7 +78,7 @@ angular.module('bestintown').run(['$templateCache', function($templateCache) {
 
 
   $templateCache.put('templates/nearby.html',
-    "<ion-view view-title=\"Nearby Hello\">\n" +
+    "<ion-view view-title=\"Nearby Please :(\">\n" +
     "    <ion-nav-buttons side=\"Primary\">\n" +
     "        <button class=\"button\" ng-click=\"vm.share()\">\n" +
     "            <i class=\"icon ion-share\"></i>\n" +
