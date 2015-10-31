c139f37ac005d21a74638258bb85d5ddda3db7ec
Craig McNamara <cmcnamara87@gmail.com>
2015-11-01 01:27:16 +1000

    Updates

--- scripts/templates.js
+++ scripts/templates.js
@@ -115,8 +115,8 @@ angular.module('bestintown').run(['$templateCache', function($templateCache) {
     "        <h2>{{ place.name }}</h2>\n" +
     "        <p>{{ place.address }}</p>\n" +
     "        <p>{{ place.distance | number:1}} km </p>\n" +
-    "        <div ng-repeat=\"rank in place.ranks\">\n" +
-    "            <a ng-click=\"vm.openCategory($event, rank.category)\" href class=\"tag\">\n" +
+    "        <div >\n" +
+    "            <a ng-repeat=\"rank in place.ranks\" ng-click=\"vm.openCategory($event, rank.category)\" href class=\"tag\">\n" +
     "                {{ rank.category.name }} #{{ rank.rank }}\n" +
     "            </a>\n" +
     "        </div>\n" +
